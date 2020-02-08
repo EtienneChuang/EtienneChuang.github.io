cf53b1f4f339c85c4d9293bc036cc8e505ad3733
etiennechuang <etiennechuang@gmail.com>
2020-02-08 08:37:13 +0800

    modify restfulAPI.py

--- py/restfulAPI.py
+++ py/restfulAPI.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
-from flask.ext.jsonpify import jsonify
+from flask_jsonpify import jsonify
 
 app = Flask(__name__)
 api = Api(app)
